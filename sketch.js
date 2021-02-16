cdacd0b6910f1b3473d1e2b95209ba952604bcad
Arsalan <67588625+cybars69@users.noreply.github.com>
2021-02-16 22:40:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,14 +11,16 @@ function draw() {
   //background(200);
   translate(width/2,height/2);
   noStroke();
-  fill(0,60);
+  fill(0,40);
   rect(-width/2,-height/2,displayWidth,displayHeight);
   textSize(32);
   fill(0,200,0);
-  xx= radius * cos(frameCount%360);
-  yy= radius * sin(frameCount%360);
+  xx= radius * cos(radians(frameCount%360));
+  yy= radius * sin(radians(frameCount%360));
   text(s,xx,yy);
   text(s,-xx,-yy);
+  text(s,-xx,yy);
+  text(s,xx,-yy);
   //ellipse(0,0,10,50);
   
 }
