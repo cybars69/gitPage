cc47e7de300d1cf9393974fb166a2d1a9d3bb192
Arsalan <67588625+cybars69@users.noreply.github.com>
2021-02-25 13:24:20 +0530

    Update p5sketch.js

--- processing/better-windows-screensaver/p5sketch.js
+++ processing/better-windows-screensaver/p5sketch.js
@@ -1,42 +1,42 @@
 var xOff = 0;
 var yOff = 40;
-var inc = -1;
-var radius = 50;
+var inc = 1;
+var radius = 20;
 var capFrame = 0;
 var size;
+var name_ ;
 
 function setup() {
-  size = Math.min(displayWidth,displayHeight)
-  createCanvas(size,size);
-  background(0);
+    size = Math.min(displayWidth, displayHeight)
+    createCanvas(size, size);
+    background(0);
 }
 
 
 function draw() {
-  //background(0);
-  if(mouseIsPressed && 
-    mouseX <= size && mouseX >=0 &&
-    mouseY <= size && mouseY >=0 &&
-    frameCount - capFrame > 120){
-    var imgName = "myCanvas_" + Math.floor(1000 + Math.random() * 9000) ;
-    saveCanvas(imgName, 'png');
-    mouseIsPressed=0;
-    capFrame = frameCount;
-  }
-  varx = map(noise(xOff),0,1,0 ,width);
-  vary = map(noise(yOff),0,1,0 ,height);
-  xOff += 0.008;
-  yOff += 0.008;
-  strokeWeight(1);
-  // fill(random(50,200),random(50,200),random(50,200));
-  fill(random(80,200),random(80,200),random(80,200))
+    if (mouseIsPressed &&
+        mouseX <= size && mouseX >= 0 &&
+        mouseY <= size && mouseY >= 0 &&
+        frameCount - capFrame > 120) {
+        name_ = new Date();
+        var imgName = "myCanvas_" + name_.getDate() + '_' + name_.getHours() + ':' +name_.getMinutes() + ':' + name_.getSeconds();
+        saveCanvas(imgName, 'png');
+        mouseIsPressed = 0;
+        capFrame = frameCount;
+    }
+    varx = map(noise(xOff), 0, 1, -width / 10, width * 1.1);
+    vary = map(noise(yOff), 0, 1, -height / 10, height * 1.1);
+    xOff += 0.008;
+    yOff += 0.008;
+    strokeWeight(1);
+    // fill(random(50,200),random(50,200),random(50,200));
+    fill(random(80, 200), random(80, 200), random(80, 200))
 
-  ellipse(varx,vary,random(radius/1.1,radius),random(radius/1.1,radius));
-  if(frameCount%3==0){
-    radius += inc;
-  }
-  if(radius>60 || radius<10){
-    inc *=-1;
-  }
-  
+    ellipse(varx, vary, random(radius / 1.1, radius), random(radius / 1.1, radius));
+    if (frameCount % 3 == 0) {
+        radius += inc;
+    }
+    if (radius > 80 || radius < 15) {
+        inc *= -1;
+    }
 }
